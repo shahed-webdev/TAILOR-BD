d8e18c3280d6cc6859c193eb9e45ce265965b646
MUHIBOLLAH <muhebollah.diu@gmail.com>
2021-12-01 16:19:29 +0600

    payments

--- AccessAdmin/quick-order/js/quick-order.js
+++ AccessAdmin/quick-order/js/quick-order.js
@@ -23,13 +23,10 @@ function initData() {
 
         //order list data
         selectedIndex: null,
-        order: [], //[{OrderDetails:'', dress: {}, measurements:[], styles:[] }],
+        order: [], //[{OrderDetails:'', dress: {}, measurements:[], styles:[], payments:[] }],
 
         //get dress dropdown
-        dressNames: {
-            isLoading: true,
-            data: []
-        },
+        dressNames: { isLoading: true,data: [] },
 
         async getDress() {
             const { customerId, clothForId } = this.apiData;
@@ -108,12 +105,43 @@ function initData() {
             }
         },
 
-        //customer
-        customer: {
-            isLoading: false,
-            isNewCustomer: true,
-            data: {}
+        //payments
+        dressPayment : { paymentFor: '', amount: '' },
+
+        //modal
+        onOpenPaymentModal(index) {
+            this.selectedIndex = index;
+            $("#addPaymentModal").modal("show");
         },
+
+        //add payment to cart
+        addPayment(index) {
+            const { paymentFor, amount } = this.dressPayment;
+            const orderPayment = this.order[index];
+            orderPayment.payments = orderPayment.payments || [];
+
+            //check payment added or not
+            const isAdded = orderPayment.payments.some(item => item.paymentFor.toLocaleLowerCase() === paymentFor.toLocaleLowerCase());
+
+            if (isAdded) {
+                $.notify(`${paymentFor} already added`, { position: "to center" }, "error");
+                return;
+            }
+
+            orderPayment.payments.push({
+                dressName: orderPayment.dress.dressName,
+                dressQuantity: orderPayment.quantity,
+                paymentFor,
+                amount,
+                paymentDressQuantity: orderPayment.quantity
+            });
+
+            //reset form
+            this.dressPayment = { paymentFor: '', amount: '' };
+        },
+
+        //customer
+        customer: { isLoading: false,isNewCustomer: true,data: {}},
 
         //find
         findCustomer(evt) {
@@ -162,13 +190,7 @@ function initData() {
         //add new
         async addNewCustomer() {
             const { Phone, CustomerName, Address, Cloth_For_ID = 1 } = this.customer.data;
-
-            const model = {
-                Phone,
-                CustomerName,
-                Address,
-                Cloth_For_ID
-            }
+            const model = { Phone,CustomerName, Address, Cloth_For_ID }
 
             try {
                 const response = await fetch(`${helpers.baseUrl}/AddNewCustomer`,
@@ -214,199 +236,3 @@ function initData() {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//get form data as object
-//const serializeForm = function (form) {
-//    const obj = {};
-//    const formData = new FormData(form);
-//    for (let key of formData.keys()) {
-//        obj[key] = formData.get(key);
-//    }
-//    return obj;
-//};
-
-
-//const order = (function () {
-    //find customer
-    //$(document).on("paste, input", ".find-customer", function () {
-    //    $(this).typeahead({
-    //        minLength: 2,
-    //        displayText: function (item) {
-    //            return `${item.CustomerName}, ${item.Phone}`;
-    //        },
-    //        afterSelect: function (item) {
-    //            this.$element[0].value = item.CustomerName
-    //        },
-    //        source: function (request, result) {
-    //            $.ajax({
-    //                url: `Order.aspx/FindCustomer?prefix=${JSON.stringify(request)}`,
-    //                contentType: "application/json; charset=utf-8",
-    //                success: response => result(response.d),
-    //                error: err => console.log(err)
-    //            });
-    //        },
-    //        updater: function (item) {
-    //            return item;
-    //        }
-    //    })
-//    })
-
-//    //add customer
-//    const addCustomerForm = document.getElementById("addCustomerForm");
-//    addCustomerForm.addEventListener("submit", function (evt) {
-//        evt.preventDefault();
-
-//        const model = serializeForm(this);
-//        console.log(model)
-//    })
-
-//    //dress name dropdown
-//    const dressDropdown = document.getElementById("dress-dropdown");
-
-//    //get dress from api
-//    function getDress(customerId=0, clothForId=0) {
-//        $.ajax({
-//            type:'GET',
-//            url: `Order.aspx/DressDlls?customerId=${customerId}&clothForId=${clothForId}`,
-//            contentType: "application/json; charset=utf-8",
-//            success: response=> {
-//                const list = response.d || [];
-//                const fragment = document.createDocumentFragment();
-
-//                if (list.length) {
-//                    list.forEach(item => fragment.appendChild(createOption(item.DressId, item.DressName)))
-
-//                    dressDropdown.appendChild(fragment)
-//                }
-//            },
-//            error: err => console.log(err)
-//        });
-//    }
-
-//    //create options
-//    function createOption(value, label) {
-//        const option = document.createElement("option");
-//        option.value = value;
-//        option.text = label;
-
-//        return option;
-//    }
-
-//    getDress();
-
-//    //on change dress dropdown
-//    dressDropdown.addEventListener("change", function (evt) {
-//        if (!evt.target.value) return;
-
-//        const customerId = 5042;
-//        const dressId = +evt.target.value;
-
-//        $.ajax({
-//            type: 'GET',
-//            url: `Order.aspx/GetDressMeasurementsStyles?dressId=${dressId}&customerId=${customerId}`,
-//            contentType: "application/json; charset=utf-8",
-//            success: response => {
-//                renderMesurement(response.d.MeasurementGroups);
-//                renderStyleCategory(response.d.StyleGroups);
-//            },
-//            error: err => console.log(err)
-//        });
-//    })
-
-//    //render mesurement
-//    const mesurements = document.getElementById("mesurements");
-//    function renderMesurement(mesurementsGroup = []) {
-//        const fragment = document.createDocumentFragment();
-
-//        mesurementsGroup.forEach(item => {
-//            const div = document.createElement("div");
-//            div.className = "col-sm-4 col-lg-3 mb-4";
-//            div.innerHTML = `<div class="card px-3 pt-2 h-100">${renderNameAndInput(item.Measurements)}</div>`;
-
-//            fragment.appendChild(div);
-//        });
-
-//        mesurements.innerHTML = "";
-//        mesurements.append(fragment);
-//    }
-
-//    //render mesurement name and input
-//    function renderNameAndInput(measurementsData=[]) {
-//        let html = '';
-//        measurementsData.forEach(item => {
-//            const active = item.Measurement ? "active" : "";
-
-//            html += `<div class="mb-2">    
-//                <div class="md-form md-outline my-1">
-//                   <label class="${active}" for="${item.MeasurementTypeID}">${item.MeasurementType}</label>
-//                  <input id="${item.MeasurementTypeID}" type="text" class="form-control" value="${item.Measurement}" autocomplete="off">
-//                </div>
-//              </div>`
-//        })
-
-//        return html;
-//    }
-
-
-//    //render styles
-//    const dressStyles = document.getElementById("dress-styles");
-//    function renderStyleCategory(styleGroups = []) {
-//        const fragment = document.createDocumentFragment();
-
-//        styleGroups.forEach(item => {
-//            const div = document.createElement("div");
-//            div.className = "col-sm-6 col-lg-4 mb-4";
-//            div.innerHTML = `
-//              <div class="card">
-//                <div class="card-header bg-white" role="tab">
-//                    <a href="#tab${item.DressStyleCategoryId}" class="collapsed black-text" data-toggle="collapse" data-parent="#dress-styles" aria-expanded="false">
-//                        <p class="mb-0">${item.DressStyleCategoryName}</p>
-//                    </a>
-//                </div>
-
-//                <div id="tab${item.DressStyleCategoryId}" class="collapse" role="tabpanel" data-parent="#dress-styles">
-//                    <div class="card-body">
-//                        ${renderStyle(item.Styles)}
-//                    </div>
-//                </div>
-//             </div>`;
-
-//            fragment.appendChild(div);
-//        });
-
-//        dressStyles.innerHTML = "";
-//        dressStyles.append(fragment);
-//    }
-
-//    //render mesurement name and input
-//    function renderStyle(styles = []) {
-//        let html = '';
-//        styles.forEach(item => {
-//            const checked = item.IsCheck ? "checked" : "";
-
-//            html += `<div class="mb-3">    
-//                <div class="custom-control custom-checkbox mb-1">
-//                  <input type="checkbox" class="custom-control-input" id="${item.DressStyleId}" ${checked}>
-//                  <label class="custom-control-label" for="${item.DressStyleId}">${item.DressStyleName}</label>
-//                </div>
-//                <input id="${item.DressStyleId}" type="text" class="form-control" value="${item.DressStyleMesurement}" autocomplete="off">
-//              </div>`
-//        })
-
-//        return html;
-//    }
-//})(document);
