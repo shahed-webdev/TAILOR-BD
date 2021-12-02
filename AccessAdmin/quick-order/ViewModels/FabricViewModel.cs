e7287581a3b37d3a610762263735fe656f747009
Shahed Al Mahmud <shahedloopsit@gmail.com>
2021-12-02 13:00:39 +0600

    fabric find web mathod added

---
+++ AccessAdmin/quick-order/ViewModels/FabricViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TailorBD.AccessAdmin.quick_order.ViewModels
+{
+    public class FabricViewModel
+    {
+        public int FabricId { get; set; }
+        public string FabricCode { get; set; }
+        public string FabricsName { get; set; }
+        public double SellingUnitPrice { get; set; }
+        public double StockFabricQuantity { get; set; }
+        public string UnitName { get; set; }
+    }
+}
