6939c24aa983912bdcfc6edfff5162eb8d43746e
Shahed Al Mahmud <shahedloopsit@gmail.com>
2021-12-09 13:09:40 +0600

    facric selling Post mathod

--- AccessAdmin/quick-order/FabricSelling.aspx.cs
+++ AccessAdmin/quick-order/FabricSelling.aspx.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TailorBD.AccessAdmin.quick_order.ViewModels;
 
 namespace TailorBD.AccessAdmin.quick_order
 {
@@ -11,6 +16,54 @@ namespace TailorBD.AccessAdmin.quick_order
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        //post order
+        [WebMethod]
+        public static ResponseModel<int> PostOrder(FabricSellingModel model)
+        {
+            try
+            {
+                var institutionId = Convert.ToInt32(HttpContext.Current.Request.Cookies["InstitutionID"]?.Value);
+                var registrationId = Convert.ToInt32(HttpContext.Current.Request.Cookies["RegistrationID"]?.Value);
+                var fabricsSellingId = 0;
+                //Insert order List
+                using (var con = new SqlConnection())
+                {
+                    con.ConnectionString = ConfigurationManager.ConnectionStrings["TailorBDConnectionString"]
+                        .ConnectionString;
+                    using (var cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = @"SP_Fabrics_Sell";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+
+                        cmd.Parameters.AddWithValue("@InstitutionID", institutionId);
+                        cmd.Parameters.AddWithValue("@RegistrationID", registrationId);
+
+
+                        cmd.Parameters.AddWithValue("@AccountID", model.AccountID);
+                        cmd.Parameters.AddWithValue("@CustomerID", model.CustomerID);
+                        cmd.Parameters.AddWithValue("@PaymentSituation", model.PaymentSituation);
+                        cmd.Parameters.AddWithValue("@SellingPaidAmount", model.SellingPaidAmount);
+                        cmd.Parameters.AddWithValue("@SellingDiscountAmount", model.SellingDiscountAmount);
+                        cmd.Parameters.AddWithValue("@FabricList", model.FabricList);
+
+                        con.Open();
+                        fabricsSellingId = Convert.ToInt32(cmd.ExecuteScalar());
+                        con.Close();
+                    }
+                }
+
+                return new ResponseModel<int>(true, "Success", fabricsSellingId);
+            }
+            catch (Exception e)
+            {
+                return new ResponseModel<int>(false, e.Message);
+            }
+
 
         }
     }
---
+++ AccessAdmin/quick-order/ViewModels/FabricSellingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TailorBD.AccessAdmin.quick_order.ViewModels
+{
+    public class FabricSellingModel
+    {
+        public int CustomerID { get; set; }
+        public int RegistrationID { get; set; }
+        public int InstitutionID { get; set; }
+        public int AccountID { get; set; }
+        public double SellingDiscountAmount { get; set; }
+        public double SellingPaidAmount { get; set; }
+        public string PaymentSituation { get; set; }
+        public string FabricList { get; set; } //[{ FabricID, SellingQuantity, SellingUnitPrice }]
+
+    }
+}
